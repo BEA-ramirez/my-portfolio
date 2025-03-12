9f0b40d36d655f0c808273ba9a46f56dc0b05d30
Bea Erin Angel L. Ramirez <21-1-01483@vsu.edu.ph>
2025-03-12 22:28:23 +0800

    main page view done

---
+++ app/main/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <div>
+        <h1>BEA Ramirez</h1>
+        <h5>Backend Developer</h5>
+        <p className="line-clamp-2">
+          I build robust backends that support innovative projects and enhance
+          user experiences.
+        </p>
+      </div>
+      <div>I am a developer that is passionate of learning and etc.</div>
+    </div>
+  );
+}
---
+++ components/NavBar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Braces } from "lucide-react";
+
+export default function NavBar() {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">About</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Skills</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Projects</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Blog</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Contacts</BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
